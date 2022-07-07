d221259e11361577be1acc7ecf01d0af246db864
ahsanali143 <96178327+ahsanali143@users.noreply.github.com>
2022-07-07 04:34:23 -0700

    Second commit

---
+++ app/src/main/java/com/reshma/beautycare/Acne.java
@@ -0,0 +1,21 @@
+package com.reshma.beautycare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class Acne extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acne);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ app/src/main/java/com/reshma/beautycare/Frfr.java
@@ -0,0 +1,15 @@
+package com.reshma.beautycare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class Frfr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frfr);
+    }
+}
